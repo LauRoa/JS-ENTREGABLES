3d88a1176dcc626be92c2386b5f3c4e60d132cb8
Lautaro Roa <lautaro.roa@hotmail.es>
2022-07-18 23:46:31 -0300

    agregado sesionStorage

--- DESAFIO COMPLEMENTARIO 3/registro.js
+++ DESAFIO COMPLEMENTARIO 3/registro.js
@@ -1,10 +1,2 @@
 let usuario;
-let usuarioStorage = localStorage.getItem("usuario");
-
-if(usuarioStorage){
-  usuario = usuarioStorage;
-  alert("Bienvenido " + usuario);
-}else{
-  usuario = prompt("Ingresa tu nombre");
-  localStorage.setItem("usuario", usuario);
-}
+let usuarioStorage = sesionStorage.getItem("usuario");
